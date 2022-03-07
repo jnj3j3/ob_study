9ba9d96f45950b20d0810be22ce6dd891410394f
= <=>
2022-03-07 20:00:00 +0900

    two

---
+++ 0315/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace yoon_year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int year = int.Parse(Console.ReadLine());
+                if (year < 1 || year > 3000)
+                {
+                    Console.WriteLine("다시입력해주세요.");
+                    continue;
+                }
+
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) Console.WriteLine(year + "년은 윤년입니다.");
+                else Console.WriteLine(year + "년은 평년입니다.");
+            }
+        }
+    }
+}
