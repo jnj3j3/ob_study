ed43d0035d82e349d7eb378bac85906b5bb174e9
= <=>
2022-03-22 20:34:00 +0900

    three

---
+++ 0322/fac.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0322
+{
+    class Fraction
+    {
+        private int numerator;
+        private int denominator;
+        public int Numerator
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+        public int Denominator
+        {
+            get { return denominator; }
+            set { denominator = value; }
+        }
+        override public string ToString()
+        {
+            return (numerator + "/" + denominator);
+        }
+    }
+
+    class fac
+    {
+        public static void main()
+        {
+            Fraction f = new Fraction(); int i;
+            f.Numerator = 1; // invoke set-accessor in Numerator
+            i = f.Numerator + 1; // invoke get-accessor in Numerator
+            f.Denominator = i; // invoke set-accessor in Denominator
+            Console.WriteLine(f.ToString());
+        }
+    }
+}
---
+++ 0322/sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _0322
+{
+    class sum
+    {
+        static void SumAvg(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++) sum = sum + arr[i];
+
+            Console.WriteLine("sum : " + sum);
+            Console.WriteLine("avg : " + sum / arr.Length);
+        }
+        static void Main(string[] args)
+        {
+            int[] abc = { 1, 2, 3, 4, 5 };
+            SumAvg(abc);
+        }
+    }
+}
