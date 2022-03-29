4a33a324e27f782d44434d17f691e660959c8ad0
= <=>
2022-03-29 20:34:00 +0900

    four

---
+++ 0329/arg.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace arg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; ++i)
+                Console.WriteLine("Argument[{0}] = {1}", i, args[i]);
+        }
+    }
+}
